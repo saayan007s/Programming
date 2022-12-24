c2a4548eb02b1a9a82357e9a67012cf0e2c4750d
saayan007s <saayan1909@gmail.com>
2022-12-24 20:07:24 +0530

    Create Template.cpp

---
+++ Template Library/Template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<long long, long long>;
+using vi = vector<int>;
+using vl = vector<long long>;
+using vpi = vector<pair<int, int>>;
+using vpl = vector<pair<long long, long long>>;
+
+#define fur(i, a, b) for(ll i = a; i <= (ll)b; ++i)
+#define ruf(i, a, b) for(ll i = a; i >= (ll)b; --i)
+#define fr first 
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define nl "\n"
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+
+void solve()
+{
+    
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+	ll t = 1;
+    /* cin >> t; */
+    while(t--)
+    {
+        solve();
+    }
+}
+
