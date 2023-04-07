f1e6e7c2bd67d033636f03b1400f9251a07ff16a
saayan007s <saayan1909@gmail.com>
2023-04-07 12:18:27 +0530

    Create min_Template.cpp
    Trying a minimal template. Will have to write for loops now. :(

---
+++ Template Library/min_Template.cpp
@@ -0,0 +1,19 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+#define fr first
+#define sc second
+#define eb emplace_back
+#define nl "\n"
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    /* cin >> t; */
+    for(int i = 1; i <= t; ++i) {
+        solve();
+    }
+}
