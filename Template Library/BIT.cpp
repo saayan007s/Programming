97a827ce604abdf701ceccb293862dad94e0cc4f
saayan007s <saayan1909@gmail.com>
2023-04-25 17:28:30 +0530

    Update BIT.cpp

--- Template Library/BIT.cpp
+++ Template Library/BIT.cpp
@@ -1,3 +1,27 @@
+struct BIT {
+    int n;
+    vector<int> a;
+
+    BIT(int N) {
+        n = N - 1;
+        a.resize(N);
+    }
+
+    void mod(int x, int v) {
+        for(; x <= n; x += x&-x) a[x] += v;
+    }
+
+    int qry(int x) {
+        int res = 0;
+        for(;x > 0; x -= x&-x) res += a[x];
+        return res;
+    }
+
+    int qry(int x, int y) {
+        return qry(y) - qry(x - 1);
+    }
+};
+
 struct FT { // fenwick tree
     vl tree;
     ll n;
