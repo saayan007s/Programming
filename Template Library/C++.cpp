0867897e0879d2dc760e3100b459746be0574e98
saayan007s <saayan1909@gmail.com>
2023-07-05 16:53:04 +0530

    General

--- Template Library/C++.cpp
+++ Template Library/C++.cpp
@@ -5,33 +5,7 @@ using namespace std;
 #define fr first
 #define sc second
 #define eb emplace_back
-const char nl = '\n';
-
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");} 
- 
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
-#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
-#else
-#define dbg(x...)
-#endif
+#define nl "\n"
 
 void solve() {
 
@@ -44,8 +18,8 @@ signed main() {
     int t = 1;
     cin >> t;
     for(int i = 1; i <= t; ++i) {
-        cout << "Case #" << i << nl;
+        /* cout << "Case #" << i << nl; */
         solve();
-        cout << nl;
+        /* cout << nl; */
     }
 }
